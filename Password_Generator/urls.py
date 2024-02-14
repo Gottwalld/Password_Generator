d98b4851553b01ae3ca97f0bced3a27936876ed5
Kirill <s.kirill10@gmail.com>
2024-02-14 22:49:50 +1000

    end  14/09/24

--- Password_Generator/urls.py
+++ Password_Generator/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', views.home),
+    path('generatedpassword/', views.password, name='password'),
 ]
---
+++ generator/views.py
@@ -0,0 +1,32 @@
+import string
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    alphabet_UP = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    nums = list('0123456789')
+    special_chars = list('!?@*')
+
+    lenght = int(request.GET.get('length', 12))
+
+    if request.GET.get('Uppercase') == "on":
+        alphabet.extend(alphabet_UP)
+    if request.GET.get('Numbers') == "on":
+        alphabet.extend(nums)
+    if request.GET.get('Special') == "on":
+        alphabet.extend(special_chars)
+
+    thepassword = ""
+    for i in range(0, lenght):
+        thepassword += random.choice(alphabet)
+    return render(request, 'generator/password.html', {'password': thepassword})
